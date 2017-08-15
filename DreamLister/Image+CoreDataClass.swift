27eef93ee26e76737c9984db489a894b94f35528
davidmgudeman <davidmgudeman@gmail.com>
2017-08-15 15:51:27 -0700

    set up the models and made NSManaged Objects and extensions

---
+++ DreamLister/Image+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Image+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by David Gudeman on 8/15/17.
+//  Copyright © 2017 dmgudeman. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Image: NSManagedObject {
+
+}
---
+++ DreamLister/ItemType+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  ItemType+CoreDataProperties.swift
+//  DreamLister
+//
+//  Created by David Gudeman on 8/15/17.
+//  Copyright © 2017 dmgudeman. All rights reserved.
+//
+
+import Foundation
+import CoreData
+ 
+
+extension ItemType {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ItemType> {
+        return NSFetchRequest<ItemType>(entityName: "ItemType");
+    }
+
+    @NSManaged public var type: String?
+    @NSManaged public var toItem: Item?
+
+}
