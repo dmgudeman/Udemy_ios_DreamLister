1f3308d6843f098be79938eb932312783bd81e6b
davidmgudeman <davidmgudeman@gmail.com>
2017-08-15 17:21:46 -0700

    hooked up the mainview and added protocols for the tableview

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by David Gudeman on 8/15/17.
+//  Copyright © 2017 dmgudeman. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+  @IBOutlet weak var thumb: UIImageView!
+  @IBOutlet weak var title: UILabel!
+  @IBOutlet weak var price: UILabel!
+  @IBOutlet weak var details: UILabel!
+}
