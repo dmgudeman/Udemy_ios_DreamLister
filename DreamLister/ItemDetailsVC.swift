7140c7f825177c88a35002c2376d257ce54cb286
davidmgudeman <davidmgudeman@gmail.com>
2017-08-16 17:02:48 -0700

    DetailsView: changed labels to custom textfields

--- DreamLister/ItemDetailsVC.swift
+++ DreamLister/ItemDetailsVC.swift
@@ -8,31 +8,47 @@
 
 import UIKit
 
-class ItemDetailsVC: UIViewController {
-
-    override func viewDidLoad() {
+class ItemDetailsVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+  
+  
+    @IBOutlet weak var storePicker: UIPickerView!
+    @IBOutlet weak var titleField: CustomTextField!
+    @IBOutlet weak var priceField: CustomTextField!
+    @IBOutlet weak var detailsField: CustomTextField!
+    
+    var stores = [Store]()
+    
+  override func viewDidLoad() {
         super.viewDidLoad()
 
       if let topItem = self.navigationController?.navigationBar.topItem {
         topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
       }
-      
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+      storePicker.delegate = self
+      storePicker.dataSource = self
     }
-    */
+
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+      let store = stores[row]
+      return store.name
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+      return stores.count
+  }
+  
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+      return 1
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+
+    // update when selected
+  }
+  
+  
+  
+  
 
 }
